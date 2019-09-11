c2ef02608d19ee659c9202e37dc4ba80fdce52f0
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-09-10 21:34:07 -0500

    Image view created

---
+++ Landmarks/circleImage.swift
@@ -0,0 +1,24 @@
+//
+//  circleImage.swift
+//  Landmarks
+//
+//  Created by Austin Meyer on 9/10/19.
+//  Copyright © 2019 Austin Meyer. All rights reserved.
+//
+
+import SwiftUI
+
+struct circleImage: View {
+    var body: some View {
+        Image("turtlerock")
+        .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius:10)
+    }
+}
+
+struct circleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        circleImage()
+    }
+}
